f25486df643bbd530cf80ce97456e886d199c950
KyleAdams26 <adams.k@ufl.edu>
2025-10-21 10:34:33 -0400

    Create setParameters.m

---
+++ setParameters.m
@@ -0,0 +1,50 @@
+function p = setParameters()
+    % Parameters of Liver Transplant model (35 parameters)
+    % This code is based on code originally written by Jaimit Parikh
+    % in 2023 and modified by Mahya Aghaee in 2024
+    
+    % parameters: 
+    p.aB2   = 1.35;       % 1
+    p.bB2   = 1e5;          % 2
+    p.aF2   = 1;         % 3
+    p.bF2   = 100;     % 4 
+    p.g2    = 0.04;      % 5
+    p.aC2   = 3;   % 6
+    p.bC2   = 250;           % 7
+    p.d2    = 0.1155;         % 8
+    p.aBF   = 0.437;          % 9
+    p.bBF   = 1e5;           %10
+    p.KF    = 1000;       %11
+    p.dF    = 1;        %12
+    p.pB    = 0.00998;         %13
+    p.KB    = 1e10;           %14
+    p.a2B   = 0.318;       %15
+    p.b2B   = 250;       %16
+    p.aFB   = 1;           %17
+    p.bFB   = 35;          %18
+    p.dB    = 2.08;        %19
+    p.aCB   = 10;         %20
+    p.bCB   = 200;           %21
+    p.aBC   = 0.25;       %22
+    p.bBC   = 1e5;       %23
+    p.a2C   = 5;       %24
+    p.b2C   = 250;      %25
+    p.aRC   = 0.3;       %26
+    p.bRC   = 200;     %27
+    p.pC    = 2.08;        %28
+    p.KC    = 598;         %29
+    p.a2LC  = 70.7;        %30
+    p.b2LC  = 99.7;        %31
+    p.aRCP  = 1;       %32
+    p.bRCP  = 200;   %33
+    p.dC    = 0.585; %34
+    p.pR    = 0.107;         %35
+    p.KR    = 1000;           %36
+    p.a2R   = 0.25;           %37
+    p.b2R   = 200;           %38
+    p.aBR   = 1.0625;           %39
+    p.bBR   = 1e5;           %40
+    p.dR    = 0.0658;           %41
+
+ 
+end
