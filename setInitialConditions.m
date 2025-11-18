2208071260ae4f52abf6cd0deb4fcde09a644937
KyleAdams26 <adams.k@ufl.edu>
2025-11-17 19:10:21 -0500

    Update setInitialConditions.m

--- setInitialConditions.m
+++ setInitialConditions.m
@@ -1,7 +1,8 @@
 function IC = setInitialConditions() %set initial conditions of state variables
-    IC.M20  = 200;
-    IC.F0  = 50;
-    IC.B0 = 2e6;
-    IC.Tc0 = 100;
-    IC.Tr0 = 100;
+    IC.M20  = 3.61e3;
+    %IC.F0  = 2112.5;
+    IC.F0 = 2.1566e5;
+    IC.B0 = 1047;
+    IC.Tc0 = 2.2254e5;
+    IC.Tr0 = 8.9112e4;
 end
