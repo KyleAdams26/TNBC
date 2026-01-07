1e66abf37e45ba72d628cf70a85f5733169b3d48
KyleAdams26 <adams.k@ufl.edu>
2026-01-07 15:38:43 -0500

    Update setParameters.m
    updated names and values

--- setParameters.m
+++ setParameters.m
@@ -1,51 +1,48 @@
 function p = setParameters()
-    % Parameters of Liver Transplant model (35 parameters)
+    % Parameters of TNBC model (39 parameters)
     % This code is based on code originally written by Jaimit Parikh
     % in 2023 and modified by Mahya Aghaee in 2024
     
     % parameters: 
-    p.aB2   = 1000;       % 1
-    p.bB2   = 5000;          % 2
+    p.aB2   = 10000;       % 1
+    p.bB2   = 1151.7;          % 2
     p.aF2   = 1.2;         % 3
-    p.bF2   = 5000000;     % 4 
-    p.g2    = 0.04;      % 5
+    p.bF2   = 397.3365;     % 4 
+    p.g2    = 0.005;      % 5
     p.aC2   = 3;   % 6
-    p.bC2   = 150000;           % 7
+    p.bC2   = 146876.4;           % 7
     p.d2    = 0.1155;         % 8
-    p.aBF   = 100000000*0.000768;          % 9
-    p.bBF   = 5000;           %10
-    p.KF    = 7691790;       %11
-    %p.KF = (.35)*261.799;
-    p.dF    = .012*0.005;        %12
-    p.pB    = 0.00998;         %13
-    p.KB    = 261799;           %14
-    p.a2B   = 0.318;       %15
-    p.b2B   = 100000;       %16
-    p.aFB   = 1.625;           %17
-    p.bFB   = 2500000;          %18
-    p.dB    = 0.0005;        %19 had to change
-    p.aCB   = 35;         %20
-    p.bCB   = 150000;           %21
-    p.aBC   = 0.136;       %22
-    p.bBC   = 5000;       %23
-    p.a2C   = 0.758;       %24
-    p.b2C   = 10000;      %25
-    p.aRC   = 0.5;       %26
-    p.bRC   = 150000;     %27
-    p.pC    = 2.08;        %28
-    p.KC    = 300000;         %29
-    p.a2LC  = 7;        %30
-    p.b2LC  = 9900.7;        %31
-    p.aRCP  = 0.71;       %32
-    p.bRCP  = 250000;   %33
-    %p.dC    = 0.585; %34
-    p.dC = 7; %changed to be inactivation rate
-    p.sR    = 57;         %35
-    p.a2R   = 0.25;           %37
-    p.b2R   = 10000;           %38
-    p.aBR   = 1.0625;           %39
-    p.bBR   = 5000;           %40
-    p.dR    = 0.0658;           %41
+    p.lF    = 0.345;          %9
+    p.KF    = 65450;          %10
+    p.dF    = .00175;         %11
+    p.pB    = 0.00998;        %12
+    p.KB    = 130900;        %13
+    p.a2B   = 0.318;          %14
+    p.b2B   = 39710;           %15
+    p.aFB   = 1.625;          %16
+    p.bFB   = 397.3365;       %17
+    p.dB    = 0.0005;         %18
+    p.aCB   = 35;             %19
+    p.bCB   = 146876.4;       %20
+    p.aBCB   = 0.136;          %21
+    p.bBCB   = 1151.7;         %22
+    p.a2CB   = 0.758;          %23
+    p.b2CB   = 39710;           %24
+    p.aRCB   = 0.5;            %25
+    p.bRCB   = 12002.32;       %26
+    p.pC    = 2.08;           %27
+    p.KC    = 222766.763199 ; %28
+    p.a2C  = 7;              %29
+    p.b2C  = 7942000;         %30
+    p.aRC  = 0.933;          %31
+    p.bRC  = 12002.32;       %32
+    p.dC    = 0.406;          %33
+    p.sR    = 583.33;         %34
+    p.a2R   = 0.25;           %35
+    p.b2R   = 39710;           %36
+    p.aBR   = 1.0625;         %37
+    p.bBR   = 1151.7;          %38
+    p.dR    = 0.063;          %39
 
  
 end
